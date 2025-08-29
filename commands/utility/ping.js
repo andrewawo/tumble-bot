d87f7dcfae67de0a1d1c0461e76c8de9fe36246b
Toba Awoniyi <tobaawoniyi@gmail.com>
2025-08-29 10:24:46 -0500

    Added 5 second cooldown to the /ping command, which was accidentally omitted in the previous commit.

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,6 +1,7 @@
 const {SlashCommandBuilder} = require('discord.js');
 
 module.exports = {
+    cooldown: 5,
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('This command replies to a user with "Pong!" when the bot receives a /ping command'),
